d0eb80c70bcced87487bf5cc1f522211829c1acd
RyanSnodgrass <res0428@yahoo.com>
2014-06-14 17:13:38 -0400

    db seed data input

--- app/models/ingredient.rb
+++ app/models/ingredient.rb
@@ -5,6 +5,7 @@ class Ingredient < ActiveRecord::Base
 	has_and_belongs_to_many :users
 	has_and_belongs_to_many :recipes
 
-	scope :liquors, -> { where(i_type: 'liquor') }
-	scope :mixers, -> { where(i_type: 'mixer') }
+	scope :liquors, -> { where(i_type: 'liquor')}
+	scope :mixers,  -> { where(i_type: 'mixer') }
+	scope :liqeurs, -> { where(i_type: 'liqeur') }
 end
