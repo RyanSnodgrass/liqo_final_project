53f0d6dbc6dcc006785e5f2f66cfcadc250f17e7
RyanSnodgrass <res0428@yahoo.com>
2014-06-14 21:05:18 -0400

    mixers adding/deleting to user's join table

--- app/controllers/mixers_controller.rb
+++ app/controllers/mixers_controller.rb
@@ -5,4 +5,27 @@ class MixersController < ApplicationController
 	def show
 		@mixer = Ingredient.find(params[:id])
 	end
+
+	# matches the clicked mixer to the db,
+	# adds it to the current_user's ingredients_users join table
+	# redirects back to previous view(should be mixers)
+	def create
+		@mixer = Ingredient.find(params[:id]) # match the mixer in the db from the mixer in the view
+		@user = current_user # set the current user
+		# @user.ingredients.build
+		# debugger
+		@user.ingredients << @mixer
+		# debugger
+		redirect_to :back
+	end
+
+	# matches the clicked mixer to the db,
+	# removes it from the current_user's ingredients_users join table
+	# redirects back to previous view(should be mixers)
+	def destroy
+		@mixer = Ingredient.find(params[:id]) # match the mixer in the db from the mixer in the view
+		@user = current_user
+		@user.ingredients.delete @mixer
+		redirect_to :back
+	end
 end
